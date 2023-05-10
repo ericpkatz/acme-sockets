22e7960ce80e6bbff692761f5d9900f0e00bde2c
ericpkatz <ericpkatz@gmail.com>
2023-05-10 19:42:38 -0400

    setup sockets

--- server/index.js
+++ server/index.js
@@ -1,11 +1,41 @@
 const app = require('./app');
-const { syncAndSeed } = require('./db');
+const { syncAndSeed, User } = require('./db');
+const ws = require('ws');
 
+const socketMap = {
+};
 const init = async()=> {
   try {
-    await syncAndSeed();
+    if(process.env.SYNC !== 'NO'){
+      await syncAndSeed();
+    }
     const port = process.env.PORT || 3000;
     const server = app.listen(port, ()=> console.log(`listening on port ${port}`));
+
+    const socketServer = new ws.WebSocketServer({ server });
+    socketServer.on('connection', (socket)=> {
+      socket.on('close', ()=> {
+        const userId = socket.userId;
+        delete socketMap[userId];
+        Object.values(socketMap).forEach(value => {
+          value.socket.send(JSON.stringify({ type: 'LOGOUT', user: { id: userId }}));
+        });
+      });
+      socket.on('message', async(data) => {
+        const message = JSON.parse(data);
+        if(message.token){
+          const user = await User.findByToken(message.token);
+          socketMap[user.id] = { socket, user};
+          socket.userId = user.id;
+
+          Object.values(socketMap).forEach( value => {
+            if(value.user.id !== user.id){
+              value.socket.send(JSON.stringify({ type: 'LOGIN', user }));
+            }
+          });
+        }
+      });
+    });
   }
   catch(ex){
     console.log(ex);
--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Home from './Home';
 import Login from './Login';
 import { useSelector, useDispatch } from 'react-redux';
@@ -8,10 +8,33 @@ import { Link, Routes, Route } from 'react-router-dom';
 
 const App = ()=> {
   const { auth } = useSelector(state => state);
+  const prevAuth = useRef(auth);
   const dispatch = useDispatch();
+
   useEffect(()=> {
     dispatch(loginWithToken());
   }, []);
+
+  useEffect(()=> {
+    if(!prevAuth.current.id && auth.id){
+      console.log('you just logged in.');
+      window.socket = new WebSocket(window.location.origin.replace('http', 'ws'));
+      window.socket.addEventListener('open', ()=> {
+        window.socket.send(JSON.stringify({ token: window.localStorage.getItem('token')}));
+      });
+      window.socket.addEventListener('message', (ev)=> {
+        const message = JSON.parse(ev.data);
+        console.log(message);
+      });
+    }
+    if(prevAuth.current.id && !auth.id){
+      window.socket.close();
+    }
+  }, [auth]);
+
+  useEffect(()=> {
+    prevAuth.current = auth;
+  });
 
   return (
     <div>
